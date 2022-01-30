cef0c3aeb5e4385270e667d8441468573863c0d8
YongJin-Jo <artinc5233@gmail.com>
2022-01-30 22:38:31 +0900

    price-component useQuery이용해 api 기능 구현 금액 view 구현 환료

--- src/Routes/Price.tsx
+++ src/Routes/Price.tsx
@@ -1,5 +1,47 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import { fetchCoinHistor } from '../api/api';
+import { CoinHistory } from '../type/CoinDefine';
+
+const PriceList = styled.ul`
+  display: flex;
+  flex-direction: column;
+`;
+const PriceInfo = styled.li`
+  margin: 10px;
+  padding: 5px;
+  border-bottom: 1px solid white;
+  span {
+    margin: 10px;
+  }
+`;
 
 export const Price = () => {
-  return <div>price</div>;
+  const { coinId } = useParams();
+  const { isLoading, data } = useQuery<CoinHistory[]>(
+    ['ohlcv', coinId],
+    () => fetchCoinHistor(coinId!),
+    { refetchInterval: 10000 }
+  );
+  return (
+    <>
+      {isLoading ? (
+        '...Loding'
+      ) : (
+        <PriceList>
+          {data?.map(price => (
+            <PriceInfo>
+              <span>날짜: {price.time_open.substring(0, 10)}</span>
+              <span>시작가: ${price.open.toFixed(1)}</span>
+              <span>마감가: ${price.close.toFixed(1)}</span>
+              <span>최저가: ${price.low.toFixed(1)}</span>
+              <span>최고가: ${price.high.toFixed(1)}</span>
+            </PriceInfo>
+          ))}
+        </PriceList>
+      )}
+    </>
+  );
 };
